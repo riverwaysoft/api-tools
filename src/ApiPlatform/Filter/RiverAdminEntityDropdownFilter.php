81421635b1227bbba257adb14900a5898f123b4d
Gorbachev Egor <7gorbachevm@gmail.com>
2022-05-27 19:09:11 +0300

    Add a flag to remove IRI from RiverAdminEntityDropdownFilter

--- src/ApiPlatform/Filter/RiverAdminEntityDropdownFilter.php
+++ src/ApiPlatform/Filter/RiverAdminEntityDropdownFilter.php
@@ -19,14 +19,34 @@ use Symfony\Component\Serializer\NameConverter\NameConverterInterface;
  * #[ApiFilter(
  *      RiverAdminEntityDropdownFilter::class,
  *      properties: ['progresses.user'],
- *      arguments: ['endpoint' => '/api/users', 'labelKey' => 'username', 'async' => false])
+ *      arguments: [
+ *          'endpoint' => '/api/users',
+ *          'labelKey' => 'username',
+ *          // Set it to true if you want to render component that loads options from a remote source as the user types: https://react-select.com/async
+ *          // Set it to false if you have a static list
+ *          'async' => false,
+ *          // Optional, false by default
+ *          // Set it to true if you don't want to remove the automatically added iri to the filter value, e.g path?query=/api/user/{id}
+ *          'removeIri' => false,
+ *      ])
  * ]
- *
- * Use 'async' => true if you want to render component that loads options from a remote source as the user types: https://react-select.com/async
  */
 class RiverAdminEntityDropdownFilter extends SearchFilter
 {
-    public function __construct(private string $endpoint, private string $labelKey, ManagerRegistry $managerRegistry, ?RequestStack $requestStack, IriConverterInterface $iriConverter, PropertyAccessorInterface $propertyAccessor = null, LoggerInterface $logger = null, array $properties = null, IdentifiersExtractorInterface $identifiersExtractor = null, NameConverterInterface $nameConverter = null,  private bool $async = false)
+    public function __construct(
+        private string $endpoint,
+        private string $labelKey,
+        ManagerRegistry $managerRegistry,
+        ?RequestStack $requestStack,
+        IriConverterInterface $iriConverter,
+        PropertyAccessorInterface $propertyAccessor = null,
+        LoggerInterface $logger = null,
+        array $properties = null,
+        IdentifiersExtractorInterface $identifiersExtractor = null,
+        NameConverterInterface $nameConverter = null,
+        private bool $async = false,
+        private bool $removeIri = false,
+    )
     {
         parent::__construct($managerRegistry, $requestStack, $iriConverter, $propertyAccessor, $logger, $properties, $identifiersExtractor, $nameConverter);
     }
@@ -41,8 +61,10 @@ class RiverAdminEntityDropdownFilter extends SearchFilter
                 $description[$property]['property'] = sprintf('riveradmin_entity_dropdown:%s', json_encode([
                     'endpoint' => $this->endpoint,
                     'labelKey' => $this->labelKey,
-                    'async' => $this->async
+                    'async' => $this->async,
+                    'removeIri' => $this->removeIri,
                 ]));
+
             }
         }
 
